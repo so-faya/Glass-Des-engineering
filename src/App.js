542dca31cb67432d5ad06d146f7bf2681c966808
so-faya <sophiaekwusie70@gmail.com>
2024-10-07 16:35:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 import Home from './pages/Home';
 import Services from './pages/Services'; // Import the Services page
 import About from './pages/About';
-import Contact from './pages/Contact'
+// import Contact from './pages/Contact'
 import NewsPage from './pages/NewsPage';
 import Cart from './pages/Cart'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes and Route
@@ -17,7 +17,7 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/About" element={<About />} />
           <Route path="/NewsPage" element={<NewsPage />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
