a8c1f04270233973cddb2f734898cb7e6786265e
so-faya <sophiaekwusie70@gmail.com>
2024-11-01 21:36:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import About from './pages/About';
 // import Contact from './pages/Contact'
 import NewsPage from './pages/NewsPage';
 import Cart from './pages/Cart'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes and Route
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/NewsPage" element={<NewsPage />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    </Router>
+    <HashRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/NewsPage" element={<NewsPage />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+
+    </HashRouter>
   );
 }
 
